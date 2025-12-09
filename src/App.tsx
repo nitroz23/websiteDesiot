51a2e9f89fbd7620d1e9bee70387b6ca97b643b0
Zia <therealnitroz23@gmail.com>
2025-12-10 02:08:24 +0700

    POST GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import bg from './assets/car-parking-scaled.jpg'
-import './App.css'
+import { useState, useEffect } from "react";
+import bg from "./assets/car-parking-scaled.jpg";
+import "./App.css";
 
 // Reusable SensorCard component
 function SensorCard({ title, value, unit, size = "normal" }) {
@@ -14,22 +14,55 @@ function SensorCard({ title, value, unit, size = "normal" }) {
   );
 }
 
-
 function App() {
+  const [temperature, setTemperature] = useState("--");
+  const [humidity, setHumidity] = useState("--");
+  const [distance, setDistance] = useState("--");
+
+  useEffect(() => {
+    const fetchSensorData = () => {
+      fetch("http://192.168.200.48:1880/data/sensor1")
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Network response was not OK");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setTemperature(data.temperature);
+          setHumidity(data.humidity);
+          setDistance(data.distance_cm);
+        })
+        .catch((err) => console.error("Fetch error:", err));
+    };
+
+    fetchSensorData();                         // initial load
+    const interval = setInterval(fetchSensorData, 2000); // repeat every 2s
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="app" style={{ backgroundImage: `url(${bg})` }}>
-      <h1>DESIOT</h1>
+      <h1>TPark - BALLS</h1>
 
-      {/* Sensor cards */}
       <section className="sensors">
         <div className="sensor-row">
-          <SensorCard title="Temperature" value="--" unit="°C" size="normal" />
-          <SensorCard title="Humidity" value="--" unit="%" size="normal"/>
+          <SensorCard title="Temperature" value={temperature} unit="°C" />
+          <SensorCard title="Humidity" value={humidity} unit="%" />
         </div>
-        <SensorCard title="Cars Detected" value="NO" unit=" " size="wide" />
+
+        <div className="sensor-row">
+          <SensorCard title="Distance" value={distance} unit="cm" />
+          <SensorCard
+            title="Car Detected"
+            value={distance < 60 ? "YES" : "NO"}
+            unit=""
+          />
+        </div>
       </section>
     </div>
   );
 }
 
-export default App
+export default App;
